7343bdfddfe9ea29c8b10270e1694ad5e3512189
KhuongTuanaanh <khuongtuananh38@gmail.com>
2025-10-17 11:15:56 +0700

    Add files via upload

---
+++ Week4/main.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main() {
+    BST tree;
+    tree.insert(50);
+    tree.insert(30);
+    tree.insert(70);
+    tree.insert(20);
+    tree.insert(40);
+    tree.insert(60);
+    tree.insert(80);
+    tree.printInorder();
+    tree.printPreorder();
+    tree.printPostorder();
+}
